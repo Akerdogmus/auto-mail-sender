b9a1eb773cbb28ede0951eb15bc993de1f7ed861
A.Kerem Erdoğmuş <akerdogmus@gmail.com>
2023-06-15 17:03:11 +0300

    Update auto_mail_sender.py
    Added
    
        Added support for using relative file paths for the sender email file and recipients file.
        Included a new function get_current_directory() to retrieve the current directory.
        Improved code readability and maintainability.
    
    Changed
    
        Updated file paths for the sender email file and recipients file in the new code.
    
    Deprecated
    
        None
    
    Removed
    
        Removed the unused import statement import os from the new code.
    
    Fixed
    
        Fixed a potential issue in the new code where the file paths were not being correctly set.
        Improved exception handling and error messages when sending emails.
    
    Security
    
        None

--- auto_mail_sender.py
+++ auto_mail_sender.py
@@ -1,57 +1,73 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# Email sender information
+# Sender email information
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
-sender_email_file = 'auto_mail_sender/sender.txt'
-recipients_file = 'auto_mail_sender/recipients.txt'
+sender_email_file = 'sender.key'
+recipients_file = 'recipients.txt'
 
-# Email subject
+# Email subject and content
 subject = 'AUTO MAIL SENDER TEST'
 
-# Read sender information from file
-with open(sender_email_file, 'r') as f:
-    sender_email = f.readline().strip()
-    sender_password = f.readline().strip()
-    print("User info read")
+def main():
 
-# Read recipient list from file and send separate emails to each recipient
-with open(recipients_file, 'r') as f:
-    recipients = [line.strip() for line in f.readlines()]
-    print("Recipients read")
+    #NOTE: IF YOU USE THIS CODE WITH VCODE, OPEN COMMENTS BOTTOM LINES.
+    """
+    file_location = get_current_directory()
+    sender_email_file = file_location + '/auto_mail_sender/sender.key'
+    recipients_file = file_location + '/auto_mail_sender/recipients.txt'
+    """
 
-    for recipient in recipients:
-        # Create email content
-        message = f"Hello {recipient},\n\nThis is a test email sent automatically."
+    # Read sender information from the file
+    with open(sender_email_file, 'r') as f:
+        sender_email = f.readline().strip()
+        sender_password = f.readline().strip()
+        print("User info read")
 
-        # Create email
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = recipient
-        msg['Subject'] = subject
+    # Read the recipient list from the file and send separate emails to each user
+    with open(recipients_file, 'r') as f:
+        recipients = [line.strip() for line in f.readlines()]
+        print("Recipients read")
 
-        msg.attach(MIMEText(message, 'plain'))
+        for recipient in recipients:
+            # Create email content
+            message = f"Hello {recipient},\n\nThis is a test email sent automatically."
 
-        print(f"Email created and sending to {recipient}.")
+            # Create the email
+            msg = MIMEMultipart()
+            msg['From'] = sender_email
+            msg['To'] = recipient
+            msg['Subject'] = subject
 
-        # Connect to the SMTP server and log in
-        try:
-            server = smtplib.SMTP(smtp_server, smtp_port)
-            server.starttls()
-            server.login(sender_email, sender_password)
+            msg.attach(MIMEText(message, 'plain'))
 
-            # Send the email
-            server.sendmail(sender_email, recipient, msg.as_string())
-            print(f"Email successfully sent to {recipient}!")
+            print(f"Email created and sending to {recipient}.")
 
-        except smtplib.SMTPException as e:
-            print(f"SMTP Error ({recipient}):", str(e))
+            # Connect to the SMTP server and log in
+            try:
+                server = smtplib.SMTP(smtp_server, smtp_port)
+                server.starttls()
+                server.login(sender_email, sender_password)
 
-        except Exception as e:
-            print(f"An error occurred while sending email to {recipient}:", str(e))
+                # Send the email
+                server.sendmail(sender_email, recipient, msg.as_string())
+                print(f"Email successfully sent to {recipient}!")
 
-        finally:
-            # Close the connection to the SMTP server
-            server.quit()
+            except smtplib.SMTPException as e:
+                print(f"SMTP Error ({recipient}):", str(e))
+
+            except Exception as e:
+                print(f"An error occurred while sending the email ({recipient}):", str(e))
+
+            finally:
+                # Close the connection to the SMTP server
+                server.quit()
+
+def get_current_directory():
+    return os.getcwd()
+
+if __name__ == '__main__':
+    main()
